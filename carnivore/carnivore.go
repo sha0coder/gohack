77f83b5744b1f783077196a5c263da2a654e8009
sha0coder <sha0@badchecksum.net>
2022-03-30 14:40:55 +0200

    false positive (fp) reductor, wg goroutines synchronization

--- carnivore/carnivore.go
+++ carnivore/carnivore.go
@@ -1,3 +1,12 @@
+/*
+	Carnivore url pentest
+		- test new parameters not found in url
+		- allow tests only spcecific parameter
+		- many payloads
+		- expert system recognize errors 
+		- gauss to false positive reduction
+*/
+
 package main
 
 import (
@@ -5,6 +14,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
+	"math"
 	"strings"
 )
 
@@ -31,6 +42,87 @@ func loadWordlist(wordlist string) []string {
 	return list
 }
 
+func injectPayloads(param, url string, params, payloads []string, curls chan<-string) {
+	if param == "" {
+		for _, p := range params {
+			for _, v := range payloads {
+				curls <- url + "&" + p + "=" + v
+			}
+		}
+	} else {
+		parts := strings.Split(url, param+"=")
+	
+		if len(parts) < 2 {
+			url += "&"+param+"=##"
+		} else {
+			value := strings.Split(parts[1], "&")
+			if value[0] == "" {
+				url += "##"
+			} else {
+				url = strings.ReplaceAll(url, value[0], "##")
+			}
+		}
+
+		for _, v := range payloads {
+			u := strings.ReplaceAll(url, "##", v)
+			curls <- u
+		}
+	}
+	close(curls)
+}
+
+func process(g int, c <-chan string, cres chan<-map[string]int, indicators []string, dbg bool, wg *sync.WaitGroup) {
+	for url := range c {
+		html, code, _ := R.Get(url)
+		if dbg {
+			fmt.Printf("[%d] sz:%d  %s\n", code, len(html), url)
+		}
+		for _, i := range indicators {
+			if strings.Contains(html, i) {
+				fmt.Printf("%s  indicator: %s\n", url, i)
+			}
+		}
+		res := make(map[string]int)
+		res[url] = len(strings.Split(html, "\n"))
+		cres <- res
+	}
+	wg.Done()
+}
+
+func reduceFPs(cres <- chan map[string]int) {
+	results := make(map[string]int)
+	mean := 0.0
+	elems := 0
+	sum := 0.0
+
+	for r := range cres {
+		for k, v := range r {
+			results[k] = v
+			mean += float64(v)
+			elems += 1
+		}
+	}
+
+	mean = mean / float64(elems)
+
+	for _, v := range results {
+		sum += math.Pow(float64(v)-mean, 2)
+	}
+	
+	err := math.Sqrt(sum/float64(elems))
+
+
+	fmt.Printf("gauss mean:%f err:%f\n", mean, err)
+	fmt.Println(" --- results ---")
+	for url, lines := range results {	
+		if float64(lines) < mean - err || float64(lines) > mean + err {
+			fmt.Printf("[%d] %s\n", lines, url)
+		}
+	}
+	fmt.Println("done.")
+	os.Exit(1)
+}
+
 func main() {
 	goroutines := flag.Int("go", 1, "number of goroutines")
 	url := flag.String("url", "", "target url")
@@ -44,61 +136,36 @@ func main() {
 	}
 
 	fmt.Println("loading ...")
-	c := make(chan string, 6)
+	curls := make(chan string, 6)
+	cres := make(chan map[string]int, 6)
+
 	indicators := loadWordlist("indicators.txt")
 	params := loadWordlist("params.txt")
 	payloads := loadWordlist("payloads.txt")
 	R = NewRequests()
+	var wg sync.WaitGroup
+	wg.Add(*goroutines)
 
 	if !strings.Contains(*url, "?") {
 		*url = *url + "?"
 	}
 
 	for i := 0; i < *goroutines; i++ {
-		go func(g int, c <-chan string, indicators []string, dbg bool) {
-			for url := range c {
-				html, code, _ := R.Get(url)
-				if dbg {
-					fmt.Printf("[%d] sz:%d  %s\n", code, len(html), url)
-				}
-				for _, i := range indicators {
-					if strings.Contains(html, i) {
-						fmt.Printf("%s  indicator: %s\n", url, i)
-					}
-				}
-			}
-		}(i, c, indicators, *dbg)
+		go process(i, curls, cres, indicators, *dbg, &wg)
 	}
 
-	if *param == "" {
-		for _, p := range params {
-			for _, v := range payloads {
-				c <- *url + "&" + p + "=" + v
-			}
-		}
-	} else {
-		parts := strings.Split(*url, *param+"=")
-	
-		if len(parts) < 2 {
-			*url += "&"+*param+"=##"
-		} else {
-			value := strings.Split(parts[1], "&")
-			if value[0] == "" {
-				*url += "##"
-			} else {
-				*url = strings.ReplaceAll(*url, value[0], "##")
-			}
-		}
+	go injectPayloads(*param,*url, params, payloads, curls)
 
-		for _, v := range payloads {
-			u := strings.ReplaceAll(*url, "##", v)
-			c <- u
-		}
-	}
 	fmt.Println("pentesting ...")
+
+	go reduceFPs(cres)
+
+	wg.Wait()
+	close(cres)
 
 	var i int
 	for {
 		fmt.Scanf("%d", &i)
 	}
 }
+
